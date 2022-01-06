3d01abc63a53c05853e333c6db6838ef544390fc
김응길 <yesroad.dev@gmail.com>
2022-01-06 17:29:31 +0900

    병렬 호출

--- basic-practice/src/components/RQParallelQueriesPage.js
+++ basic-practice/src/components/RQParallelQueriesPage.js
@@ -5,21 +5,34 @@ import { useQuery } from 'react-query';
 // 참고 : https://react-query.tanstack.com/guides/parallel-queries
 
 const fetchTodos = async () => {
-    const { data } = await axios.get('http://localhost:4000/todos');
-    return data;
+	const { data } = await axios.get('http://localhost:4000/todos');
+	return data;
 };
 
 const fetchFriends = async () => {
-    const { data } = await axios.get('http://localhost:4000/friends');
-    return data;
+	const { data } = await axios.get('http://localhost:4000/friends');
+	return data;
 };
 
 const ParallelQueriesPage = () => {
-    // "Parallel" queries는 병렬로 실행되거나 동시성을 극대화하기 위해 실행되는 쿼리이다. 
-    // 아래의 쿼리는 병렬로 실행된다. 
-    // TODO
+	// "Parallel" queries는 병렬로 실행되거나 동시성을 극대화하기 위해 실행되는 쿼리이다.
+	// 아래의 쿼리는 병렬로 실행된다.
+	// TODO
 
-    return <div></div>;
+	const { data: todos } = useQuery('todos', fetchTodos);
+	const { data: friends } = useQuery('friends', fetchFriends);
+
+	return (
+		<div>
+			{todos?.map((todo) => (
+				<div key={todo.id}>{todo.title}</div>
+			))}
+			<div>---</div>
+			{friends?.map((friend) => (
+				<div key={friend.id}>{friend.name}</div>
+			))}
+		</div>
+	);
 };
 
 export default ParallelQueriesPage;
