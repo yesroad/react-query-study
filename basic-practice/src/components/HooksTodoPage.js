d83e336ba5467b83c74264b6b105d5a8c65a92be
김응길 <yesroad.dev@gmail.com>
2022-01-06 17:22:06 +0900

    react hook 을 사용하여 api 호출

--- basic-practice/src/components/HooksTodoPage.js
+++ basic-practice/src/components/HooksTodoPage.js
@@ -1,17 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 const HooksToDoPage = () => {
-    // TODO useEffect, useState Hook을 사용하여 서버에서 데이터 가져오기
+	// TODO useEffect, useState Hook을 사용하여 서버에서 데이터 가져오기
+	const [loading, setLoading] = useState(false);
+	const [todos, setTodos] = useState(null);
+	const [error, setError] = useState(false);
 
-    return (
-        <>
-            <h2>To Do</h2>
-            {/* {todos.map((todo) => {
-                return <div key={todo.id}>{todo.title}</div>;
-            })} */}
-        </>
-    )
-}
+	const getList = useCallback(async () => {
+		setLoading(true);
+		try {
+			const response = await axios.get('http://localhost:4000/todos');
+			setTodos(response.data);
+		} catch (e) {
+			setError(e);
+		}
+		setLoading(false);
+	}, []);
 
-export default HooksToDoPage
+	useEffect(() => {
+		getList();
+	}, [getList]);
+
+	if (loading) return <div>로딩</div>;
+	if (error) return <div>에러</div>;
+
+	return (
+		<>
+			<h2>To Do</h2>
+			{todos?.map((todo) => {
+				return <div key={todo.id}>{todo.title}</div>;
+			})}
+		</>
+	);
+};
+
+export default HooksToDoPage;
